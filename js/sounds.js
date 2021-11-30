07c844ef4f5c59cf70478c6c8c3b5b3d33f6d8f3
ethyl2 <zzzuzz@gmail.com>
2021-11-29 21:32:27 -0700

    Moves more sound functionality to sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -5,3 +5,25 @@ const beginningSounds = [
   { name: 'timer', type: 'wav' },
   { name: 'creep', type: 'wav' }
 ]
+
+// Get user's latest audio preferences
+let audio
+const audioCheckboxEl = document.getElementById('allow-audio')
+const allowAudio = JSON.parse(window.localStorage.getItem('audio_permission'))
+if (allowAudio) {
+  audioCheckboxEl.checked = true
+} else {
+  audioCheckboxEl.checked = false
+}
+
+audioCheckboxEl.addEventListener('change', (e) => {
+  if (e.target.checked) {
+      window.localStorage.setItem('audio_permission', true)
+      if (audio) {
+          audio.play()
+      }
+  } else {
+      window.localStorage.setItem('audio_permission', false)
+      audio.pause()
+  }
+})
