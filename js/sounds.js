d456cd6a1364f1100eed46f7d8981ce1b27f111d
ethyl2 <zzzuzz@gmail.com>
2021-11-29 21:27:28 -0700

    Moves sounds array to its own file

---
+++ js/sounds.js
@@ -0,0 +1,7 @@
+const beginningSounds = [
+  { name: 'whispers', type: 'wav' },
+  { name: 'laugh', type: 'wav' },
+  { name: 'organ', type: 'wav' },
+  { name: 'timer', type: 'wav' },
+  { name: 'creep', type: 'wav' }
+]
